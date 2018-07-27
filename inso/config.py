cce04d405f3dc9c79539d9460e4575cc5b86ee76
Jasper <36561872+agapius@users.noreply.github.com>
2018-07-27 21:50:44 +0200

    Add files via upload

---
+++ inso/config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+	SECRET_KEY = '7a4af642e0eec14ef9d49bdf833f9091'
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = 'insolvenz.app@gmail.com'
+	MAIL_PASSWORD = 'insolvenz'
+
+#move values to environment variable
+# 'insolvenz.app@gmail.com'
+#'insolvenz'
+#7a4af642e0eec14ef9d49bdf833f9091
+#sqlite:///site.db
---
+++ inso/main/routes.py
@@ -0,0 +1,16 @@
+from flask import render_template, request, Blueprint
+from inso.models import Post
+
+main = Blueprint('main', __name__)
+
+
+
+
+@main.route("/")
+@main.route("/home")
+def home():
+	return render_template('home.html', title='Home')
+
+@main.route("/about")
+def about():
+	return render_template('about.html', title='About')
