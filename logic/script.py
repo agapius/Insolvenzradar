6024ceef37e1b1369a855fb835b176852b2a2e86
d-e-h-i-o <niklas.dehio@gmail.com>
2018-08-10 10:31:28 +0200

    some small changes

--- logic/script.py
+++ logic/script.py
@@ -1,8 +1,10 @@
 import time
+import datetime
 
 # this script is constantly running. it mainy builds on the inso.py
 
 while True:
+	log = open('log.txt', 'w')
 	latest_post = load_latest_post() 	        # this loads the latest bekanntmachung in the database
 	updates = update_database(latest_post)		# this scrapes all bekanntmachungen after latest bekanntmachung, inserts them in database, and returns the data for analysis
 	update_counter = 0
@@ -14,5 +16,6 @@ while True:
 			if verfahren[1] in u_verfahren:
 				send_mail(user, verfahren)
 				update_counter += 1
+				log_data = 'Mail send to' + user + ',' + 'verfahren' + '' + str(datetime.datetime.now())
 
 	time.sleep(43200) 					        # script sleeps for 24 hours
