897fabea484afe926b1c2d5c745fa37992f532be
Jasper <36561872+agapius@users.noreply.github.com>
2018-08-11 14:39:01 +0200

    Update utils.py

--- inso/users/utils.py
+++ inso/users/utils.py
@@ -21,15 +21,13 @@ def save_picture(form_picture):
 
 
 def send_reset_email(user):
-    token = user.get_reset_token()
-    msg = Message('Password Reset Request',
-                  sender='noreply@demo.com',
-                  recipients=[user.email])
-    msg.body = '''To reset your password, visit the following link:
+	token = user.get_reset_token()
+	msg = Message('Password Reset Request', sender='insolvenz.app@gmail.com', recipients=[user.email])
+	msg.body = f'''To reset your Password, visit the following link:
 {url_for('users.reset_token', token=token, _external=True)}
-If you did not make this request then simply ignore this email and no changes will be made.
-'''.format(url_for('users.reset_token', token=token, _external=True))
-mail.send(msg)
+If you did not make this request, simply ignore this Email and no changes will be made.
+'''
+	mail.send(msg)
 
 
 
