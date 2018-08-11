db613bfa7e09f2e99f692fc96064dbb9ff97d5eb
Jasper <36561872+agapius@users.noreply.github.com>
2018-08-11 13:55:24 +0200

    Update utils.py

--- inso/users/utils.py
+++ inso/users/utils.py
@@ -25,10 +25,10 @@ def send_reset_email(user):
     msg = Message('Password Reset Request',
                   sender='noreply@demo.com',
                   recipients=[user.email])
-    msg.body = f'''To reset your password, visit the following link:
+    msg.body = '''To reset your password, visit the following link:
 {url_for('users.reset_token', token=token, _external=True)}
 If you did not make this request then simply ignore this email and no changes will be made.
-'''
+'''.format(url_for('users.reset_token', token=token, _external=True))
 mail.send(msg)
 
 
