850df061305c9361528b6d9d773008ee62742b09
d-e-h-i-o <niklas.dehio@gmail.com>
2018-08-22 15:44:45 +0200

    added error handling to insertion

--- logic/script_mysql.py
+++ logic/script_mysql.py
@@ -173,7 +173,11 @@ def insert_into_database(data_from_page):
 			for verfahren in data_from_page:
 				query = "INSERT INTO inso (regno, datum, inhaber, ort, link, full_string, bekanntmachung) VALUES (\'{}\',\'{}\',\'{}\',\'{}\',\'{}\',\'{}\',\'{}\')".format(verfahren[0], verfahren[1], verfahren[2], verfahren[3], verfahren[4], verfahren[5], verfahren[6])
 				#print(query)
-				cursor.execute(query)
+				try:
+					cursor.execute(query)
+				except Exception as y:
+					print(y)
+					print(query)
 			connection.commit()
 		#connection.close()
 	except Exception as e:
